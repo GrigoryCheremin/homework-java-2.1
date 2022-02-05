fc7ba1d1b2ef4e72cbb74859c59d332ed85cab12
GrigoryCheremin <Greenx.git.work@gmail.com>
2022-02-05 10:58:12 +0300

    added file

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int startBal = 100; // начальный баланс, руб.
+        int amount = 100; // сумма пополнения счёта, руб.
+        int lim = 1000; // сумма, после которой начисляются бонусы, руб.
+        int bonus = amount / 100; // количество бонусных рублей
+        int finalBalance;
+        if (amount > lim) {
+            finalBalance = startBal + amount + bonus;
+            System.out.println("Итого на счёте:");
+            System.out.println(finalBalance);
+            System.out.println("Количество бонусных рублей:");
+            System.out.println(bonus);
+        }
+        else {
+            finalBalance = startBal + amount;
+            System.out.println("Итого на счёте:");
+            System.out.println(finalBalance);
+            System.out.println("Количество бонусных рублей 0");
+        }
+    }
+}
