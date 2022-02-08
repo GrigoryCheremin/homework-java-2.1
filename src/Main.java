69dff27287c685165bbd7fb89cab91f77818193e
GrigoryCheremin <Greenx.git.work@gmail.com>
2022-02-08 19:32:34 +0300

    reformate code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int startBal = 100; // начальный баланс, руб.
-        int amount = 100; // сумма пополнения счёта, руб.
+        int amount = 2100; // сумма пополнения счёта, руб.
         int lim = 1000; // сумма, после которой начисляются бонусы, руб.
         int bonus = amount / 100; // количество бонусных рублей
         int finalBalance;
@@ -11,8 +11,7 @@ public class Main {
             System.out.println(finalBalance);
             System.out.println("Количество бонусных рублей:");
             System.out.println(bonus);
-        }
-        else {
+        } else {
             finalBalance = startBal + amount;
             System.out.println("Итого на счёте:");
             System.out.println(finalBalance);
